2a53f66a86e8b20542ab574d23e19673cc185ce0
liao <liao892826990@gmail.com>
2025-07-08 17:11:47 +0800

    商品功能-refactor category,使用Enum類型

---
+++ mall/src/main/java/com/liao/mall/constant/MyTest.java
@@ -0,0 +1,12 @@
+package com.liao.mall.constant;
+
+public class MyTest {
+    public static void main(String[] args) {
+        ProductCategory category = ProductCategory.FOOD;
+        String s = category.name();
+        System.out.println(s);
+
+        System s2 = "CAR";
+        ProductCategory category2 = ProductCategory.valueOf(s2);
+    }
+}
